bc9a1297e5f4043ef698e7f2d5c38b0e303ad531
Ennis <itszedam@gmail.com>
2025-04-12 00:38:48 +0100

    Changed include

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import {PrismaClient} from "@prisma/client/extension";
+import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const app = express();
