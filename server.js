257d223af7edf2d62b55eeb23bf33a411e9055b3
Ennis <itszedam@gmail.com>
2025-04-12 00:26:26 +0100

    Still same error yay

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient} from "@prisma/client/extension";
 const prisma = new PrismaClient();
 
 const app = express();
